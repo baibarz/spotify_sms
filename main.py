f0d203d5081fa83d3a0db952a782be0c972606eb
bebarz <m.msteaf@gmail.com>
2023-12-18 00:51:53 +0100

    d

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import threading
 import time
-from sms_handler import init_modem, send_at_command, delete_sms
+from sms_handler import init_modem, send_at_command, delete_sms, send_sms_response
 from spotify_controller import search_and_play, is_track_playing
 from queue import Queue
 
@@ -12,13 +12,45 @@ current_song_playing = False
 def check_for_new_sms():
     global current_song_playing
     while True:
-        # ... (implement the logic from your original code)
+        messages = send_at_command('AT+CMGL="ALL"')
+        for i, msg in enumerate(messages):
+            try:
+                decoded_msg = msg.decode('utf-8', 'ignore').strip()
+                if '+CMGL' in decoded_msg:
+                    parts = decoded_msg.split(',')
+                    if len(parts) > 1 and '"REC UNREAD"' in parts[1]:
+                        index = parts[0].split(':')[1].strip()
+                        sender_number = parts[2].replace('"', '')
+                        sms_content = messages[i + 1].decode('utf-8', 'ignore').strip()
+                        handle_sms(sms_content, sender_number)
+                        delete_sms(index)
+            except IndexError as e:
+                print(f"IndexError in message parsing: {e}")
+            except Exception as e:
+                print(f"Error in check_for_new_sms: {e}")
         time.sleep(2)
+
+def handle_sms(sms_content, sender_number):
+    global current_song_playing
+    if 'skip' in sms_content.lower():
+        # Code to skip current track
+        send_sms_response("Skipping current track.", sender_number)
+    elif 'remove' in sms_content.lower():
+        # Code to remove specific track from queue
+        send_sms_response("Removing specified track from queue.", sender_number)
+    else:
+        song_queue.put((sms_content, sender_number))
+        send_sms_response("Your request is added to the queue.", sender_number)
 
 def play_music_from_queue():
     global current_song_playing
     while True:
-        # ... (implement the logic from your original code)
+        if not current_song_playing and not song_queue.empty():
+            song_request, _ = song_queue.get()
+            search_and_play(song_request)
+            current_song_playing = True
+        elif current_song_playing and not is_track_playing():
+            current_song_playing = False
         time.sleep(1)
 
 # Initialize modem
--- spotify_controller.py
+++ spotify_controller.py
@@ -1,3 +1,4 @@
+
 # spotify_controller.py
 
 import spotipy
@@ -6,9 +7,39 @@ from queue import Queue
 
 # Spotify Credentials and Setup
 cache_path = '/home/spotify_sms/.spotipy_cache'
-SPOTIFY_CLIENT_ID = 'your_spotify_client_id'  # Replace with your Spotify Client ID
-SPOTIFY_CLIENT_SECRET = 'your_spotify_client_secret'  # Replace with your Spotify Client Secret
-SPOTIFY_REDIRECT_URI = 'http://localhost:8888/callback'  # Replace with your Spotify Redirect URI
+SPOTIFY_CLIENT_ID = '2978daaf8a6845b9bd5036c89fe33179'
+SPOTIFY_CLIENT_SECRET = '37d229b68df74e4ba187be68b61d9ff3'
+SPOTIFY_REDIRECT_URI = 'http://localhost:8888/callback'
+
+# Initialize Spotify client
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
+                                               client_secret=SPOTIFY_CLIENT_SECRET,
+                                               redirect_uri=SPOTIFY_REDIRECT_URI,
+                                               scope='user-read-playback-state user-modify-playback-state'))
+
+def is_track_playing():
+    try:
+        playback_state = sp.current_playback()
+        return playback_state is not None and playback_state['is_playing']
+    except Exception as e:
+        print(f"Error checking playback state: {e}")
+        return False
+
+def search_and_play(query):
+    try:
+        if "playlist:" in query:
+            # Extract playlist URI and play
+            playlist_uri = query.split("playlist:")[1].strip()
+            sp.start_playback(context_uri=playlist_uri)
+        else:
+            # Search for a track
+            results = sp.search(q=query, limit=1, type='track')
+            tracks = results['tracks']['items']
+            if tracks:
+                track_uri = tracks[0]['uri']
+                sp.start_playback(uris=[track_uri])
+    except Exception as e:
+        print(f"Error in search_and_play: {e}")
 
 # Initialize Spotify client
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
